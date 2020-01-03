70b4cd3b9fcbcf84a2f4051ff8b1f04fc5043329
YutaSaeki <yusaeki@yusaeki-macbook-air.local>
2020-01-03 23:17:04 +0900

    add database connect

--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,39 @@
 const functions = require('firebase-functions');
 const express = require('express');
-
+const admin = require('firebase-admin');
+const cors = require('cors')
 const app = express();
-app.get('/api/japan', (request, response) => {
-  const res = {"country": "Japan"}
-  response.send(res);
+app.use(cors())
+
+var serviceAccount = require("./foonyansample-firebase-adminsdk-bbrr1-738e207fc4");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+var db = admin.firestore();
+var docRef = db.collection('foonyan').doc('test');
+
+app.get('/api/country', (request, response) => {
+  var res;
+  res = docRef.get()
+    .then(doc => {
+      if (!doc.exists) {
+        console.log('No such document!');
+      } else {
+        console.log('Document data:', doc.data());
+        response.send(doc.data());
+      }
+    })
+    .catch((err) => {
+        console.log('Error getting documents', err);
+    });
 })
 
-app.get('/api/australia', (request, response) => {
-  const res = {"country": "Australia"}
+app.post('/api/country', (request, response) => {
+  var res;
+  console.log(request.body);
+  docRef.set(request.body);
   response.send(res);
 })
 
